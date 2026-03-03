70b097b04d837d2b482f0957d59ffcf7240a6605
Iskandar <iskandar.13.aburahmonov@gmail.com>
2026-03-03 12:20:09 +0500

    initial commit

---
+++ src/app/org-selection/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import {OrganizationList} from "@clerk/clerk-react";
+
+export default function OrgSelectionPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-background">
+            <OrganizationList
+                hidePersonal
+                afterCreateOrganizationUrl="/"
+                afterSelectOrganizationUrl="/"
+                appearance={{
+                    elements: {
+                        rootBox: "mx-auto",
+                        card: "shadow-lg"
+                    }
+                }}
+            />
+        </div>
+    )
+}
